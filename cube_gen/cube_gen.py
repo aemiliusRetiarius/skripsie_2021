f5664a80deae6c0e584131795d819ce329a1202d
Emile Visser <emile.visser@gmail.com>
2021-08-19 16:27:56 +0200

    Added cube and  distance definitions

--- cube_gen/cube_gen.py
+++ cube_gen/cube_gen.py
@@ -1,2 +1,41 @@
 import numpy as np
 import pandas as pd
+
+
+# Front Face: (5x5)
+# 21--25
+# |   |
+# 1 --5
+#
+# Rear Face: (5x5)
+# 46--50
+# |   |
+# 26--30
+#
+# Left Face: (5x3)
+#
+# 63--65
+# |   |
+# 51--53
+#
+# Right Face: (5x3)
+#
+# 78--80
+# |   |
+# 66--68
+#
+# Top Face: (3x3)
+#
+# 87--89
+# |   |
+# 81--83
+#
+# Bottom Face: (3x3)
+#
+# 96--98
+# |   |
+# 90--92
+
+
+def distance(a, b):
+    return
