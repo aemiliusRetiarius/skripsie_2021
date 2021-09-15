d7f6004fcee7b7441c46b24fba0b1cfcf3c7a03f
Emile Visser <emile.visser@gmail.com>
2021-09-15 13:27:09 +0200

    Start of rewrite, defined graph axes

--- reconstruct_performance_analysis/performance_analysis.py
+++ reconstruct_performance_analysis/performance_analysis.py
@@ -14,36 +14,28 @@ sys.path.insert(0, sibling_path)
 
 from reconstruct_script import reconstruct
 
+import multiprocessing as mp
+import itertools
+
 import matplotlib.pyplot as plt
+import time
 
-#error_array = np.zeros((1, 10))
-noise_array = np.array([1, 5, 9, 14, 18, 23, 27, 32, 36, 41, 45, 49, 54, 59, 63, 68, 72, 77, 81, 86, 90])
-error_array = np.zeros((1,21))
-noise_array = noise_array[np.newaxis, :]
-print(error_array.shape)
-print(noise_array.shape)
-for i in range(21):
-    print("iter:", i)
-    dist_df = gen_dist_df(98, noise_array[0,i], verbosity=1)
-    error_array[0,i] = reconstruct(dist_df, verbosity=1, err_ord='rel')
-    dist_df = gen_dist_df(98, noise_array[0,i], verbosity=1)
-    error_array[0,i] = (error_array[0,i] + reconstruct(dist_df, verbosity=1, err_ord='rel'))
-    dist_df = gen_dist_df(98, noise_array[0,i], verbosity=1)
-    error_array[0,i] = (error_array[0,i] + reconstruct(dist_df, verbosity=1, err_ord='rel'))
-    dist_df = gen_dist_df(98, noise_array[0,i], verbosity=1)
-    error_array[0,i] = (error_array[0,i] + reconstruct(dist_df, verbosity=1, err_ord='rel'))
-    dist_df = gen_dist_df(98, noise_array[0,i], verbosity=1)
-    error_array[0,i] = (error_array[0,i] + reconstruct(dist_df, verbosity=1, err_ord='rel'))
-    dist_df = gen_dist_df(98, noise_array[0,i], verbosity=1)
-    error_array[0,i] = (error_array[0,i] + reconstruct(dist_df, verbosity=1, err_ord='rel'))
-    dist_df = gen_dist_df(98, noise_array[0,i], verbosity=1)
-    error_array[0,i] = (error_array[0,i] + reconstruct(dist_df, verbosity=1, err_ord='rel'))
-    dist_df = gen_dist_df(98, noise_array[0,i], verbosity=1)
-    error_array[0,i] = (error_array[0,i] + reconstruct(dist_df, verbosity=1, err_ord='rel'))
-    dist_df = gen_dist_df(98, noise_array[0,i], verbosity=1)
-    error_array[0,i] = error_array[0,i] /8
-    print("filled error array")
+###############
+##Globals
 
-print(error_array)
-plt.plot(noise_array[0,:], error_array[0,:])
-plt.show()
+#intercon_axis = np.arange(5, 98, 2)
+intercon_start = 91
+intercon_end = 98
+intercon_step = 2
+intercon_axis = np.arange(intercon_start, intercon_end, intercon_step)
+
+error_start = 0
+error_end = 21
+error_step = 10
+error_axis = np.arange(error_start, error_end, error_step)
+
+noise_start = 0
+noise_end = 51
+noise_step = 5
+noise_axis = np.arange(noise_start, noise_end, noise_step)
+##############
