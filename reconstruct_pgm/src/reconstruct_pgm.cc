24d3b7d350470effbb8aad92cf53a6cef0bc7cd4
Emile Visser <emile.visser@gmail.com>
2021-09-27 17:16:08 +0200

    Added factors def, sigma factors def, joint observ

--- reconstruct_pgm/src/reconstruct_pgm.cc
+++ reconstruct_pgm/src/reconstruct_pgm.cc
@@ -7,6 +7,8 @@
 // standard headers
 #include <iostream>  // cout, endl, flush, cin, cerr
 #include <string>  // string, stod
+#include <cmath>   // sqrt
+
 
 #include "discretetable.hpp"
 #include "prlite_zfstream.hpp"
@@ -18,6 +20,9 @@ using namespace emdw;
 
 #include "emdw.hpp"
 #include "sqrtmvg.hpp"
+
+RVIds getVariableSubset(unsigned pointNum, const vector<unsigned> &inputSource, const vector<unsigned> &inputTarget);
+double getDist(double x1, double y1, double z1, double x2, double y2, double z2);
 
 int main(int, char *argv[]) {
 
@@ -92,38 +97,111 @@ int main(int, char *argv[]) {
   // define random varibles
   // for every point have x, y, z
   // e.g. to find point i_y: (i-1)*3 +1
+  // after 3d data, add rvs for distances
   RVIds theVarsFull = {};
+  RVIds theVarsDists = {};
   for(unsigned i = 0; i < 3*(numPoints); i++)
   {
-    theVarsFull.push_back(i);
+    theVarsFull.push_back(i); // 3d rvs
+  }
+  for(unsigned i = 0; i < numRecords; i++)
+  {
+    theVarsFull.push_back(3*numPoints + i); // dist rvs
+    theVarsDists.push_back(3*numPoints + i);
   }
 
   vector< rcptr<Factor> > factors;
+  vector< rcptr<Factor> > old_factors;
+
+  // define initial gaussian parameters
+  prlite::ColVector<double> theMn(6);
+  prlite::RowMatrix<double> theCv(6,6);
+  theCv.assignToAll(0.0);
+
+  for(unsigned i = 0; i < 6; i++)
+  {
+    theMn[i] = 0.0;
+    theCv(i,i) = 1.0;
+  }
 
   // create factors
   RVIds theVarsSubset = {};
-  unsigned RVIndexBase = 0;
+  //unsigned RVIndexBase = 0;
   for(unsigned i = 0; i < numRecords; i++)
   {
-    theVarsSubset.clear();
+    // get variable subset of record
+    theVarsSubset = getVariableSubset(i, inputSource, inputTarget);
 
-    // add source x,y,z rvs to subset
-    RVIndexBase = 3*(inputSource[i] - 1);
-    theVarsSubset.push_back(RVIndexBase);
-    theVarsSubset.push_back(RVIndexBase+1);
-    theVarsSubset.push_back(RVIndexBase);
-
-    // add target x,y,z rvs to subset
-    RVIndexBase = 3*(inputSource[i] - 1);
-    theVarsSubset.push_back(RVIndexBase);
-    theVarsSubset.push_back(RVIndexBase+1);
-    theVarsSubset.push_back(RVIndexBase);
-
-
-
+    // construct gaussians and append to factor list
+    rcptr<SqrtMVG> pdfSGPtr ( new SqrtMVG(theVarsSubset, theMn, theCv));
+    rcptr<Factor> pdfPtr = pdfSGPtr;
+    factors.push_back(pdfPtr);
   }
 
+  // step through factors and add sigmapoints
+  unsigned index = 0; 
+  old_factors.clear();
+  for(rcptr<Factor> factor : factors)
+  {
+    // 6 x 13 matrix of sigma points
+    prlite::ColMatrix<double> sigmaPoints = dynamic_pointer_cast<SqrtMVG>(factor)->getSigmaPoints();
+
+    // get variables factor is defined for
+    RVIds theVarsSubset = dynamic_pointer_cast<SqrtMVG>(factor)->getVars();
+
+    // define dist row
+    prlite::ColMatrix<double> sigmaPointsDists(1, 13);
+    prlite::ColMatrix<double> sigmaPointsNoise(1, 13);
+    sigmaPointsNoise.assignToAll(0.0); // neccesary?
+
+    // calc dist row
+    for(unsigned i = 0; i < 13; i++)
+    {
+      sigmaPointsDists(0, i) = getDist(sigmaPoints(0,i), sigmaPoints(1,i), sigmaPoints(2,i), sigmaPoints(3,i), sigmaPoints(4,i), sigmaPoints(5,i));
+    }
+
+    // define dist rv
+    RVIds theVarsDist = {3*numPoints + index};
+    
+    // reconstruct new gaussian
+    rcptr<SqrtMVG> pdfSigmaSGPtr(SqrtMVG::constructFromSigmaPoints(theVarsSubset, sigmaPoints, theVarsDist, sigmaPointsDists, sigmaPointsNoise));
+    rcptr<Factor> pdfSigmaPtr = pdfSigmaSGPtr;
+
+    // redefine factor and push into old factors
+    old_factors.push_back(factor);
+    factor = pdfSigmaPtr;
+
+    // increment record index
+    index++;
+  }
+
+  //observe and reduce joint of reconsructed gaussians
+  rcptr<Factor> jointFactorPtr = absorb(factors)->observeAndReduce(theVarsDists, inputDists);
   
+  //cout << *factors[2] << endl;
+
 
 
 } // main
+
+RVIds getVariableSubset(unsigned factorNum, const vector<unsigned> &inputSource, const vector<unsigned> &inputTarget)
+{
+  RVIds theVarsSubset = {};
+
+  unsigned RVIndexBase = 3*(inputSource[factorNum] - 1);
+  theVarsSubset.push_back(RVIndexBase);
+  theVarsSubset.push_back(RVIndexBase+1);
+  theVarsSubset.push_back(RVIndexBase+2);
+
+  RVIndexBase = 3*(inputTarget[factorNum] - 1);
+  theVarsSubset.push_back(RVIndexBase);
+  theVarsSubset.push_back(RVIndexBase+1);
+  theVarsSubset.push_back(RVIndexBase+2);
+
+  return theVarsSubset;
+}
+
+double getDist(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+  return sqrt(x1*x1 + y1*y1 + z1*z1 + x2*x2 + y2*y2 + z2*z2);
+}
