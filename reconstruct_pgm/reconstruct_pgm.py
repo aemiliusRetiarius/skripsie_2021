fcc37db709ba1ed6939e793581ea5b0499e87713
Emile Visser <emile.visser@gmail.com>
2021-10-06 14:15:25 +0200

    Working pipeline

--- reconstruct_pgm/reconstruct_pgm.py
+++ reconstruct_pgm/reconstruct_pgm.py
@@ -6,24 +6,43 @@ from matplotlib import cm
 import alphashape as alsh
 
 import subprocess
-import os
+import os, sys
 
-res_df = pd.read_csv("./result.csv")
+sibling_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'cube_gen')
+sys.path.insert(0, sibling_path)
+
+from cube_gen import get_point_coords, gen_dist_df
+
+program_path = './build/src/reconstruct_pgm'
+priorPos_path = './Data/initpos10.csv'
+dists_path = './Data/dists_inter_5_noise_1%.csv'
+results_path = "./Data/result.csv"
+subprocess.run([program_path, "--p", priorPos_path, "--d", dists_path, "--r", results_path, "-l", "0.8", "-t", "1"])
+
+res_df = pd.read_csv(results_path, index_col=0)
 print(res_df)
+print(res_df.keys())
 res = np.zeros((98,3))
 
 for i in range(98):
-    res[i,0] = res_df["x"][i]
-    res[i,1] = res_df["y"][i]
-    res[i,2] = res_df["z"][i]
+    res[i,0] = res_df["x_pos"][i]
+    res[i,1] = res_df["y_pos"][i]
+    res[i,2] = res_df["z_pos"][i]
 
 fig = plt.figure()
 ax = plt.axes(projection="3d")
 
-ax.scatter3D(res[:, 0], res[:, 1], res[:, 2], c=res[:, 2], cmap='hsv')
-plt.show()
+#x.scatter3D(res[:, 0], res[:, 1], res[:, 2], c=res[:, 2], cmap='hsv')
+#plt.show()
 
-#res_list = list(map(tuple, res))
-#alpha_shape = alsh.alphashape(res_list, 0.01)
-#x.plot_trisurf(*zip(*alpha_shape.vertices), triangles=alpha_shape.faces, cmap=cm.coolwarm)
+true_points_full = get_point_coords().to_numpy()
+
+true_points = true_points_full[:,1:4]
+dif = res - true_points
+print(np.linalg.norm(dif))
+print((np.linalg.norm(dif)) / np.linalg.norm(true_points))
+
+res_list = list(map(tuple, res))
+alpha_shape = alsh.alphashape(res_list, 0.01)
+ax.plot_trisurf(*zip(*alpha_shape.vertices), triangles=alpha_shape.faces, cmap=cm.coolwarm)
 #plt.show()
