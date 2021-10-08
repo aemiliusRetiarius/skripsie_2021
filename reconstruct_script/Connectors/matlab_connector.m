a393b9716dad0f7e048bc47186cfe0d2e51d90ee
Emile Visser <emile.visser@gmail.com>
2021-10-08 17:41:15 +0200

    Reverted path change

--- reconstruct_script/Connectors/matlab_connector.m
+++ reconstruct_script/Connectors/matlab_connector.m
@@ -1,4 +1,4 @@
 load Data/edm_raw;
 lamda = 1;
 semidefiniteRelaxation(dist_mat, mask_mat, lamda);
-save(strcat(pwd, '/Data/recon_edm.mat'),'ans');
+save(strcat(pwd, '/reconstruct_script/Data/recon_edm.mat'),'ans');
