86ccab6356525cbed5b048c2be14b4579b6ed66e
Emile Visser <emile.visser@gmail.com>
2021-10-09 13:13:25 +0200

    Added function to observe point positions

--- reconstruct_pgm_perf_analysis/performance_analysis_i_initStd.py
+++ reconstruct_pgm_perf_analysis/performance_analysis_i_initStd.py
@@ -33,3 +33,36 @@ initStd_axis = np.arange(initStd_start, initStd_end, initStd_step)
 target_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Data", "z_inter_1_98_init_std_0_101_rel_1.mat")
 ##############
 
+def observe_positions(pos_df):
+    pos_df.iloc[0,1] = 0
+    pos_df.iloc[0,2] = 0
+    pos_df.iloc[0,3] = 0
+    pos_df.iloc[0,4] = 0
+    pos_df.iloc[0,5] = 0
+    pos_df.iloc[0,6] = 0
+    # point5
+    pos_df.iloc[4,1] = 100
+    pos_df.iloc[4,2] = 0 #
+    pos_df.iloc[4,3] = 0 #
+    pos_df.iloc[4,4] = 0
+    pos_df.iloc[4,5] = 0 #
+    pos_df.iloc[4,6] = 0 #
+    # point21
+    pos_df.iloc[20,1] = 0 #
+    pos_df.iloc[20,2] = 0 #
+    pos_df.iloc[20,3] = 100
+    pos_df.iloc[20,4] = 0 #
+    pos_df.iloc[20,5] = 0 #
+    pos_df.iloc[20,6] = 0
+    # point30
+    pos_df.iloc[29,1] = 0 #
+    pos_df.iloc[29,2] = 100
+    pos_df.iloc[29,3] = 0 #
+    pos_df.iloc[29,4] = 0 #
+    pos_df.iloc[29,5] = 0
+    pos_df.iloc[29,6] = 0 #
+
+    return pos_df
+
+pos_df = get_point_coords(50)
+pos_df = observe_positions(pos_df)
