c1cd5e87a18978a431cba88d4642c28f4aa61be2
Emile Visser <emile.visser@gmail.com>
2021-11-10 15:25:31 +0200

    Added script heu iden implementation

--- reconstruct_script_perf_analysis/heuristic_identification.py
+++ reconstruct_script_perf_analysis/heuristic_identification.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import scipy
 
 import os
 import sys
@@ -14,6 +15,11 @@ sys.path.insert(0, sibling_path)
 
 from reconstruct_script import reconstruct
 
+sibling_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'common_tools')
+sys.path.insert(0, sibling_path)
+
+from common_tools import heuristic_iden_reduction
+
 import matplotlib.pyplot as plt
 
 def get_normalized_error(old_dist, new_dist):
@@ -21,57 +27,62 @@ def get_normalized_error(old_dist, new_dist):
 
 index_list = []
 old_error_list = []
-less_error_list = []
-error_list = []
+full_less_error_list = []
+half_less_error_list = []
+quarter_less_error_list = []
+error = 10
 
-for i in range(46, 98, 3):
+fig = plt.figure()
+ax = plt.axes()
+
+for i in range(1, 62, 4): #1 62 4
     index_list.append(i)
-    dist_df = gen_dist_df(98, i, error_percent=15, verbosity=1)
+    print("inter:", i)
+    dist_df = gen_dist_df(98, i, noise_percent=1, error_percent=error, verbosity=0)
 
-    old_error, res_points = reconstruct(dist_df, err_ord='rel', ret_points=True, verbosity=1)
-
-    dist_df['new_dist'] = dist_df.apply(lambda row: (np.linalg.norm(res_points[(int(row['source']) -1), :]- res_points[(int(row['target'])) -1, :])), axis=1)
-    dist_df['normalized_error'] = dist_df.apply(lambda row: get_normalized_error(row['dist'], row['new_dist']), axis=1)
-    dist_df.sort_values(by='normalized_error', ascending=False, inplace=True)
-    #dist_df.reset_index(drop=True, inplace=True)
-    #print(dist_df.head)
-    dist_df.reset_index(inplace=True, drop=True)
-    print("error:", old_error)
-    print('wrong record perc:', dist_df['changed'].value_counts(normalize=True) * 100)
-    test_df = dist_df[dist_df.index < len(dist_df.index)*0.15]
-    print('wrong perc in upper 15 perc:', test_df['changed'].value_counts(normalize=True) * 100)
-    #dist_df['normalized_error'].plot()
-    #plt.show()
+    old_error, res_points = reconstruct(dist_df, err_ord='edm_rel', ret_points=True, verbosity=0)
     old_error_list.append(old_error)
+    print("old errror:",old_error)
 
-    less_dist_df = dist_df.copy()
-    less_dist_df = less_dist_df[dist_df.index >= len(dist_df.index)*0.075]
-    less_dist_df.drop(columns=['new_dist', 'normalized_error'], inplace=True)
-    rel_error, res_points = reconstruct(less_dist_df, err_ord='rel', ret_points=True, verbosity=1)
-    less_dist_df['new_dist'] = less_dist_df.apply(lambda row: (np.linalg.norm(res_points[(int(row['source']) -1), :]- res_points[(int(row['target'])) -1, :])), axis=1)
-    less_dist_df['normalized_error'] = less_dist_df.apply(lambda row: get_normalized_error(row['dist'], row['new_dist']), axis=1)
-    less_dist_df.sort_values(by='normalized_error', ascending=False, inplace=True)
-    less_dist_df.reset_index(inplace=True , drop=True)
-    print("error:", rel_error)
-    print('wrong record perc:', less_dist_df['changed'].value_counts(normalize=True) * 100)
-    #dist_df['normalized_error'].plot()
-    less_error_list.append(rel_error)
+    full_less_df = heuristic_iden_reduction(dist_df.copy(deep=True), res_points, err_to_rem=error/100)
+    full_less_error = reconstruct(full_less_df, err_ord='edm_rel', verbosity=0)
+    full_less_error_list.append(full_less_error)
+    print("full reduced errror:",full_less_error)
 
-    dist_df = dist_df[dist_df.index >= len(dist_df.index)*0.15]
-    dist_df.drop(columns=['new_dist', 'normalized_error'], inplace=True)
-    rel_error, res_points = reconstruct(dist_df, err_ord='rel', ret_points=True, verbosity=1)
-    dist_df['new_dist'] = dist_df.apply(lambda row: (np.linalg.norm(res_points[(int(row['source']) -1), :]- res_points[(int(row['target'])) -1, :])), axis=1)
-    dist_df['normalized_error'] = dist_df.apply(lambda row: get_normalized_error(row['dist'], row['new_dist']), axis=1)
-    dist_df.sort_values(by='normalized_error', ascending=False, inplace=True)
-    dist_df.reset_index(inplace=True , drop=True)
-    print("error:", rel_error)
-    print('wrong record perc:', dist_df['changed'].value_counts(normalize=True) * 100)
-    #dist_df['normalized_error'].plot()
-    error_list.append(rel_error)
+    half_less_df = heuristic_iden_reduction(dist_df.copy(deep=True), res_points, err_to_rem=(error/100)*0.5)
+    half_less_error = reconstruct(half_less_df, err_ord='edm_rel', verbosity=0)
+    half_less_error_list.append(half_less_error)
+    print("half reduced errror:",half_less_error)
 
+    quarter_less_df = heuristic_iden_reduction(dist_df.copy(deep=True), res_points, err_to_rem=(error/100)*0.25)
+    quarter_less_error = reconstruct(quarter_less_df, err_ord='edm_rel', verbosity=0)
+    quarter_less_error_list.append(quarter_less_error)
+    print("quarter reduced errror:",quarter_less_error)
     #plt.show()
 
-plt.plot(index_list, old_error_list, color= 'olive')
-plt.plot(index_list, less_error_list, color= 'red')
-plt.plot(index_list, error_list)
+old_error_array = np.array(old_error_list)
+target_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Data", "z_inter_1_62_4_noi_1_err_10_edm_rel_2.mat")
+scipy.io.savemat(target_path, dict(old_error_array=old_error_array))
+print(old_error_array)
+
+full_less_error_array = np.array(full_less_error_list)
+target_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Data", "z_inter_1_62_4_noi_1_err_10_rem_full_edm_rel_2.mat")
+scipy.io.savemat(target_path, dict(full_less_error_array=full_less_error_array))
+print(full_less_error_array)
+
+half_less_error_array = np.array(half_less_error_list)
+target_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Data", "z_inter_1_62_4_noi_1_err_10_rem_half_edm_rel_2.mat")
+scipy.io.savemat(target_path, dict(half_less_error_array=half_less_error_array))
+print(half_less_error_array)
+
+quarter_less_error_array = np.array(quarter_less_error_list)
+target_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Data", "z_inter_1_62_4_noi_1_err_10_rem_quar_edm_rel_2.mat")
+scipy.io.savemat(target_path, dict(quarter_less_error_array=quarter_less_error_array))
+print(quarter_less_error_array)
+
+plt.plot(index_list, old_error_list, color= 'red', label="Base Error")
+plt.plot(index_list, full_less_error_list, color= 'navy', label="Full Removal")
+plt.plot(index_list, half_less_error_list, color= 'royalblue', label="Half Removal")
+plt.plot(index_list, quarter_less_error_list, color= 'lightsteelblue', label="Quarter Removal")
+ax.set_ylim([0, 1.1])
 plt.show()
